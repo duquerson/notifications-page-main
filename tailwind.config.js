3382848c71f280b1e64a4f98ed04b6caf87acc13
Duquerson <sotodjgato@hotmail.com>
2022-11-01 17:53:38 -0300

    agregando estilos personalizado version mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        plus: ['Plus Jakarta Sans', "sans-serif"],      
+      },
+      colors: {
+        /**primarios */
+      
+        rojo: "hsl(1, 90%, 64%)",
+        azul: "hsl(219, 85%, 26%)",
+      
+        /**neutral */
+        azul_oscuro: "hsl(224, 21%, 14%)",
+        azul_gris_oscuro: "hsl(219, 12%, 42%)",
+        azul_gris: "hsl(219, 14%, 63%)",
+        gris1: "hsl(211, 68%, 94%)",
+        gris2: "hsl(205, 33%, 90%)",
+        gris_claro: "hsl(210, 60%, 98%)",
+      },
+      
+      /**altura personalizada */
+      height:{
+        '90': '21rem',
+
+      },
+      spacing:{
+        '132': '8.61rem',
+        '142': '42%'
+      },
+      transitionDuration:{
+        '1500': '1500ms'
+      },
+    },
   },
   plugins: [],
 }
